93909af656eb1a5264291e1f234222157af6bc9b
Slava Polyakov <pol_80@mail.ru>
2023-02-06 01:20:45 +0300

    add HW_2

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,51 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
+func Unpack(str string) (string, error) {
 	// Place your code here.
+	runeString := []rune(str)
+	result := ""
+	lastChar := ""
+	counterNumber := 0
+
+	for i := 1; i <= len(runeString); i++ {
+		nextSymbol := runeString[i-1 : i]
+
+		//Проверка на число
+		if _, err := strconv.Atoi(string(nextSymbol)); err == nil {
+			counterNumber++
+			if counterNumber > 1 {
+				panic(ErrInvalidString)
+				break
+			} else if lastChar == "" {
+				panic(ErrInvalidString)
+				break
+			} else if string(nextSymbol) == "0" {
+				result = result[:len(result)-1]
+				continue
+			}
+
+			repeat, _ := strconv.Atoi(string(nextSymbol))
+			for ii := 1; ii < repeat; ii++ {
+				result += lastChar
+			}
+
+		} else {
+			counterNumber = 0
+			if nextSymbol[0] == '\n' {
+				lastChar = "\\n"
+				result += lastChar
+			} else {
+				lastChar = string(nextSymbol)
+				result += lastChar
+			}
+		}
+	}
+
 	return "", nil
 }
